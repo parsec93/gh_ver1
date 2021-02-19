21defe18e811a4bbed5a5d1b330b3e9c1da34b39
geunho <rnjs8657@naver.com>
2021-02-20 01:24:31 +0900

    Initial commit

---
+++ src/main/java/com/geunho/gh_ver1/GhVer1Application.java
@@ -0,0 +1,13 @@
+package com.geunho.gh_ver1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhVer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhVer1Application.class, args);
+	}
+
+}
